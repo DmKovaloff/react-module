454e38a8c8472e32b3bd44f0cb05346f74128594
DmKovaloff <kovalov.dmy@gmail.com>
2024-10-31 16:46:26 +0200

    draft homework-1-2p

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Products from "./components/products/products";
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <Products/>
+        </div>
+    );
 }
 
 export default App;
